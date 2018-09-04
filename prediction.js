957e33fab59389da8a0f77fe34669d71f1fcd7fc
Matthieu Bournat <matthieu.bournat@modisfrance.fr>
2018-09-04 10:12:27 +0200

    Add logs and fix an output

--- prediction.js
+++ prediction.js
@@ -36,27 +36,27 @@ network.train([
 
     // Saison 2017/2018 - J1
     {input: { MHR: 1, SU_Agen: 0 }, output: {  MHR: 1 } },
-    {input: { Stade_Français: 0, LOU: 1 }, output: {  LOU: 1 } },
+    {input: { Stade_Francais: 0, LOU: 1 }, output: {  LOU: 1 } },
     {input: { Racing: 1, Castres: 0 }, output: {  Racing: 1 } },
     {input: { UBB: 1, ASM: 1 }, output: {  UBB: 1 } },
     {input: { RCT: 1, Section_Paloise: 0 }, output: { RCT: 1 } },
 
     // Saison 2017/2018 - J2
     {input: { SU_Agen: 1, Racing: 0 }, output: { SU_Agen: 1 } },
-    {input: { Stade_Français: 1, Stade_Rochelais: 0 }, output: { Stade_Français: 1 } },
+    {input: { Stade_Francais: 1, Stade_Rochelais: 0 }, output: { Stade_Francais: 1 } },
     {input: { Castres: 1, UBB: 0 }, output: { Castres: 1 } },
     {input: { Stade_Toulousain: 1, Section_Paloise: 0.5 }, output: { Stade_Toulousain: 1 } },
     {input: { ASM: 1, RCT: 0 }, output: { ASM: 1 } },
 
     // Saison 2017/2018 - J3
     {input: { Stade_Rochelais: 1, ASM: 0 }, output: { Stade_Rochelais: 1 } },
-    {input: { UBB: 1, Stade_Français: 0 }, output: { UBB: 1 } },
+    {input: { UBB: 1, Stade_Francais: 0 }, output: { UBB: 1 } },
     {input: { Castres: 0, MHR: 1 }, output: { MHR: 1 } },
     {input: { Section_Paloise: 1, LOU: 0 }, output: { Section_Paloise: 1 } },
     {input: { RCT: 1, Stade_Toulousain: 0 }, output: { RCT: 1 } },
 
     /// Saison 2017/2018 - J4
-    {input: { Stade_Toulousain: 1, Stade_Français: 0 }, output: { Stade_Toulousain: 1 } },
+    {input: { Stade_Toulousain: 1, Stade_Francais: 0 }, output: { Stade_Toulousain: 1 } },
     {input: { SU_Agen: 0, Stade_Rochelais: 1 }, output: { Stade_Rochelais: 1 } },
     {input: { LOU: 1, UBB: 0 }, output: { LOU: 1 } },
     {input: { Section_Paloise: 1, Castres: 0 }, output: { Section_Paloise: 1 } },
@@ -67,17 +67,17 @@ network.train([
     {input: { SU_Agen: 0, Section_Paloise: 1 }, output: { Section_Paloise: 1 } },
     {input: { UBB: 1, MHR: 0 }, output: { UBB: 1 } },
     {input: { LOU: 1, Castres: 0 }, output: { LOU: 1 } },
-    {input: { Stade_Français: 0, RCT: 1 }, output: { RCT: 1 } },
+    {input: { Stade_Francais: 0, RCT: 1 }, output: { RCT: 1 } },
 
     // Saison 2017/2018 - J6
     {input: { RCT: 1, Stade_Rochelais: 0 }, output: { RCT: 1 } },
-    {input: { Section_Paloise: 0, Stade_Français: 1 }, output: { Stade_Français: 1 } },
+    {input: { Section_Paloise: 0, Stade_Francais: 1 }, output: { Stade_Francais: 1 } },
     {input: { Stade_Toulousain: 1, SU_Agen: 0 }, output: { Stade_Toulousain: 1 } },
     {input: { Racing: 0, LOU: 1 }, output: { LOU: 1 } },
     {input: { Castres: 1, ASM: 0 }, output: { Castres: 1 } },
 
     // Saison 2017/2018 - J7
-    {input: { Stade_Français: 1, MHR: 0 }, output: { Stade_Français: 1 } },
+    {input: { Stade_Francais: 1, MHR: 0 }, output: { Stade_Francais: 1 } },
     {input: { UBB: 1, RCT: 0 }, output: { UBB: 1 } },
     {input: { SU_Agen: 0, LOU: 1 }, output: { LOU: 1 } },
     {input: { Stade_Rochelais: 1, Racing: 0 }, output: { Stade_Rochelais: 1 } },
@@ -86,7 +86,7 @@ network.train([
     // Saison 2017/2018 - J8
     {input: { Section_Paloise: 0, MHR: 1 }, output: { MHR: 1 } },
     {input: { Racing: 1, UBB: 0 }, output: { Racing: 1 } },
-    {input: { ASM: 1, Stade_Français: 0 }, output: { ASM: 1 } },
+    {input: { ASM: 1, Stade_Francais: 0 }, output: { ASM: 1 } },
     {input: { Castres: 1, Agen: 0 }, output: { Castres: 1 } },
     {input: { Stade_Rochelais: 1, Stade_Toulousain: 0 }, output: { Stade_Rochelais: 1 } },
 
@@ -106,7 +106,7 @@ network.train([
 
     // Saison 2017/2018 - J11
     {input: { Castres: 1, RCT: 0 }, output: { Castres: 1 } },
-    {input: { SU_Agen: 1, Stade_Français: 0 }, output: { SU_Agen: 1 } },
+    {input: { SU_Agen: 1, Stade_Francais: 0 }, output: { SU_Agen: 1 } },
     {input: { Stade_Rochelais: 1, Section_Paloise: 0 }, output: { Stade_Rochelais: 1 } },
     {input: { Racing: 1, MHR: 0 }, output: { Racing: 1 } },
     {input: { LOU: 0, Stade_Toulousain: 1 }, output: { Stade_Toulousain: 1 } },
@@ -117,18 +117,18 @@ network.train([
     {input: { Stade_Toulousain: 0, Castres: 1 }, output: { Castres: 1 } },
     {input: { RCT: 1, LOU: 0 }, output: { RCT: 1 } },
     {input: { ASM: 1, SU_Agen: 0 }, output: { ASM: 1 } },
-    {input: { Stade_Français: 1, Racing: 0 }, output: { Stade_Français: 1 } },
+    {input: { Stade_Francais: 1, Racing: 0 }, output: { Stade_Francais: 1 } },
 
     // Saison 2017/2018 - J13
     {input: { Racing: 1, Stade_Toulousain: 0 }, output: { Racing: 1 } },
     {input: { UBB: 1, Stade_Rochelais: 0 }, output: { UBB: 1 } },
     {input: { MHR: 1, LOU: 0 }, output: { MHR: 1 } },
-    {input: { Castres: 1, Stade_Français: 0 }, output: { Castres: 1 } },
+    {input: { Castres: 1, Stade_Francais: 0 }, output: { Castres: 1 } },
     {input: { Section_Paloise: 1, ASM: 0 }, output: { Section_Paloise: 1 } },
 
     // Saison 2017/2018 - J14
     {input: { Stade_Rochelais: 1, SU_Agen: 0 }, output: { Stade_Rochelais: 1 } },
-    {input: { Stade_Français: 1, UBB: 0 }, output: { Stade_Français: 1 } },
+    {input: { Stade_Francais: 1, UBB: 0 }, output: { Stade_Francais: 1 } },
     {input: { Stade_Toulousain: 1, RCT: 0 }, output: { Stade_Toulousain: 1 } },
     {input: { LOU: 1, Section_Paloise: 0 }, output: { LOU: 1 } },
     {input: { ASM: 0, Castres: 1 }, output: { Castres: 1 } },
@@ -136,7 +136,7 @@ network.train([
     // Saison 2017/2018 - J15
     {input: { UBB: 1, LOU: 0 }, output: { UBB: 1 } },
     {input: { SU_Agen: 1, Castres: 0 }, output: { SU_Agen: 1 } },
-    {input: { MHR: 1, Stade_Français: 0 }, output: { MHR: 1 } },
+    {input: { MHR: 1, Stade_Francais: 0 }, output: { MHR: 1 } },
     {input: { Section_Paloise: 1, Stade_Toulousain: 0 }, output: { Section_Paloise: 1 } },
     {input: { Racing: 1, ASM: 0 }, output: { Racing: 1 } },
 
@@ -144,11 +144,11 @@ network.train([
     {input: { RCT: 1, UBB: 0 }, output: { RCT: 1 } },
     {input: { LOU: 1, Agen: 0 }, output: { LOU: 1 } },
     {input: { Castres: 0, Racing: 1 }, output: { Racing: 1 } },
-    {input: { Stade_Français: 0, Section_Paloise: 1 }, output: { Section_Paloise: 1 } },
+    {input: { Stade_Francais: 0, Section_Paloise: 1 }, output: { Section_Paloise: 1 } },
     {input: { ASM: 0, MHR: 1 }, output: { MHR: 1 } },
 
     // Saison 2017/2018 - J17
-    {input: { RCT: 1, Stade_Français: 0 }, output: { RCT: 1 } },
+    {input: { RCT: 1, Stade_Francais: 0 }, output: { RCT: 1 } },
     {input: { SU_Agen: 0, Stade_Toulousain: 1 }, output: { Stade_Toulousain: 1 } },
     {input: { LOU: 1, ASM: 0 }, output: { LOU: 1 } },
     {input: { UBB: 0, Castres: 1 }, output: { Castres: 1 } },
@@ -157,7 +157,7 @@ network.train([
     // Saison 2017/2018 - J18
     {input: { MHR: 1, UBB: 0 }, output: { MHR: 1 } },
     {input: { Castres: 1, LOU: 0 }, output: { Castres: 1 } },
-    {input: { Stade_Français: 0, SU_Agen: 1 }, output: { SU_Agen: 1 } },
+    {input: { Stade_Francais: 0, SU_Agen: 1 }, output: { SU_Agen: 1 } },
     {input: { Section_Paloise: 1, Racing: 0 }, output: { Section_Paloise: 1 } },
     {input: { Stade_Rochelais: 0, RCT: 1 }, output: { RCT: 1 } },
 
@@ -172,11 +172,11 @@ network.train([
     {input: { MHR: 1, Racing: 0 }, output: { MHR: 1 } },
     {input: { Section_Paloise: 1, Stade_Rochelais: 0 }, output: { Section_Paloise: 1 } },
     {input: { RCT: 1, SU_Agen: 0 }, output: { RCT: 1 } },
-    {input: { Stade_Français: 1, Castres: 0 }, output: { Stade_Français: 1 } },
+    {input: { Stade_Francais: 1, Castres: 0 }, output: { Stade_Francais: 1 } },
     {input: { Stade_Toulousain: 0, LOU: 1 }, output: { LOU: 1 } },
 
     // Saison 2017/2018 - J21
-    {input: { Racing: 1, Stade_Français: 0 }, output: { Racing: 1 } },
+    {input: { Racing: 1, Stade_Francais: 0 }, output: { Racing: 1 } },
     {input: { SU_Agen: 0, UBB: 1 }, output: { UBB: 1 } },
     {input: { Stade_Rochelais: 1, LOU: 0 }, output: { Stade_Rochelais: 1 } },
     {input: { ASM: 1, Section_Paloise: 0 }, output: { ASM: 1 } },
@@ -184,7 +184,7 @@ network.train([
 
     // Saison 2017/2018 - J22
     {input: { Stade_Rochelais: 1, UBB: 0 }, output: { Stade_Rochelais: 1 } },
-    {input: { Stade_Français: 0, Stade_Toulousain: 1 }, output: { Stade_Toulousain: 1 } },
+    {input: { Stade_Francais: 0, Stade_Toulousain: 1 }, output: { Stade_Toulousain: 1 } },
     {input: { MHR: 1, Castres: 0 }, output: { MHR: 1 } },
     {input: { RCT: 1, ASM: 0 }, output: { RCT: 1 } },
     {input: { LOU: 0, Racing: 1 }, output: { Racing: 1 } },
@@ -192,7 +192,7 @@ network.train([
     // Saison 2017/2018 - J23
     {input: { Castres: 1, Stade_Toulousain: 0 }, output: { Castres: 1 } },
     {input: { UBB: 1, Section_Paloise: 0 }, output: { UBB: 1 } },
-    {input: { Stade_Français: 1, ASM: 0 }, output: { Stade_Français: 1 } },
+    {input: { Stade_Francais: 1, ASM: 0 }, output: { Stade_Francais: 1 } },
     {input: { MHR: 1, Stade_Rochelais: 0 }, output: { MHR: 1 } },
     {input: { Racing: 1, RCT: 0 }, output: { Racing: 1 } },
 
@@ -200,7 +200,7 @@ network.train([
     {input: { RCT: 1, MHR: 0 }, output: { RCT: 1 } },
     {input: { ASM: 1, UBB: 0 }, output: { ASM: 1 } },
     {input: { Section_Paloise: 0, SU_Agen: 1 }, output: { SU_Agen: 1 } },
-    {input: { LOU: 1, Stade_Français: 0 }, output: { LOU: 1 } },
+    {input: { LOU: 1, Stade_Francais: 0 }, output: { LOU: 1 } },
     {input: { Stade_Rochelais: 0, Castres: 1}, output: { Castres: 1 } },
     {input: { Stade_Toulousain: 1, Racing: 0}, output: { Stade_Toulousain: 1 } },
 
@@ -212,7 +212,7 @@ network.train([
     {input: { Stade_Toulousain: 1, Stade_Rochelais: 0}, output: { Stade_Toulousain: 1 } },
 
     // Saison 2017/2018 - J26
-    {input: { Stade_Rochelais: 1, Stade_Français: 0 }, output: { Stade_Rochelais: 1 } },
+    {input: { Stade_Rochelais: 1, Stade_Francais: 0 }, output: { Stade_Rochelais: 1 } },
     {input: { LOU: 1, MHR: 0 }, output: { LOU: 1 } },
     {input: { Section_Paloise: 1, RCT: 0 }, output: { Section_Paloise: 1 } },
     {input: { Racing: 1, SU_Agen: 0 }, output: { Racing: 1 } },
@@ -232,7 +232,7 @@ network.train([
     /** ------------------------------------------------------------------------------------ **/
 
     // Saison 2018/2019 - J1
-    {input: { Perpignan: 0, Stade_Français: 0 }, output: { Stade_Français: 1 } },
+    {input: { Perpignan: 0, Stade_Francais: 0 }, output: { Stade_Francais: 1 } },
     {input: { UBB: 1, Section_Paloise: 0 }, output: { UBB: 1 } },
     {input: { ASM: 1, SU_Agen: 0 }, output: { ASM: 1 } },
     {input: { LOU: 0.5, Stade_Toulousain: 0.5 }, output: { Tie: 1 } },
@@ -244,12 +244,16 @@ network.train([
     {input: { Section_Paloise: 1, RCT: 0 }, output: { Section_Paloise: 1 } },
     {input: { Castres: 1, LOU: 0 }, output: { Castres: 1 } },
     {input: { MHR: 1, Stade_Rochelais: 0 }, output: { MHR: 1 } },
-    {input: { Stade_Français: 1, UBB: 0 }, output: { Stade_Français: 1 } },
+    {input: { Stade_Francais: 1, UBB: 0 }, output: { Stade_Francais: 1 } },
     {input: { Grenoble: 0, Stade_Toulousain: 0 }, output: { Stade_Toulousain: 1 } },
     {input: { SU_Agen: 1, Perpignan: 0 }, output: { SU_Agen: 1 } },
     {input: { Racing: 0, ASM: 1 }, output: { ASM: 1 } }
 
-]);
+], {
+    // Configuartion
+    iterations: 20000,
+    log: true
+});
 
 // Prédiction Racing vs ASM - J2 
 const output = network.run({ Racing: 0.5, ASM: 0.5 });
